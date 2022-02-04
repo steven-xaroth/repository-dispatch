5bbacec4eb6adfb4bfc88ee6938b1fed06491108
Steven Noorbergen <steven.xaroth@ccpgames.com>
2022-02-04 10:02:56 +0100

    Add app-authentication support.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {createAppAuth} from '@octokit/auth-app'
 import {inspect} from 'util'
 
 async function run(): Promise<void> {
   try {
     const inputs = {
       token: core.getInput('token'),
+      appId: core.getInput('app-id'),
+      appPrivateKey: core.getInput('app-private-key'),
+      appInstallationId: core.getInput('app-installation-id'),
       repository: core.getInput('repository'),
       eventType: core.getInput('event-type'),
       clientPayload: core.getInput('client-payload')
@@ -14,7 +18,29 @@ async function run(): Promise<void> {
 
     const [owner, repo] = inputs.repository.split('/')
 
-    const octokit = github.getOctokit(inputs.token)
+    let auth: any = undefined
+    if (
+      !!inputs.appId &&
+      !!inputs.appPrivateKey &&
+      !!inputs.appInstallationId
+    ) {
+      auth = {
+        appId: inputs.appId,
+        installationId: inputs.appInstallationId,
+        privateKey: inputs.appPrivateKey
+      }
+      inputs.token = ''
+    }
+    const authStrategy = auth ? createAppAuth : undefined
+
+    if (!inputs.token && !auth) {
+      core.setFailed(
+        'either the "token" input must be set, or the "app-*" inputs.'
+      )
+      return
+    }
+
+    const octokit = github.getOctokit(inputs.token, {authStrategy, auth})
 
     await octokit.rest.repos.createDispatchEvent({
       owner: owner,
